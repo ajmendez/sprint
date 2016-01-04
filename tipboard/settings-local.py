3fbdcb085a4aa3ab247ed169b85b940db8099edc
Alexander Mendez <bluespace@gmail.com>
2016-01-04 12:40:31 -0500

    adding tipboard

---
+++ tipboard/settings-local.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+PROJECT_NAME = 'dashboard'
+API_KEY = '7f1b8f2f3d174f218784703fe4d7d455'
+
